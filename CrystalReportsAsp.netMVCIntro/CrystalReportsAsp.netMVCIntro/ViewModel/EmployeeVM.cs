db87dc2ee70d8cb4e7fdd7d5ab321d7f26f68506
japhlet <etyangemodojaphlet@yahoo.com>
2021-02-09 18:18:05 +0300

    "Added EmployeeVM.cs"

---
+++ CrystalReportsAsp.netMVCIntro/CrystalReportsAsp.netMVCIntro/ViewModel/EmployeeVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CrystalReportsAsp.netMVCIntro.ViewModel
+{
+    public class EmployeeVM
+    {
+        public int EmployeeId { get; set; }
+
+        [StringLength(15)]
+        [DisplayName("First Name*")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(15)]
+        [DisplayName("Last Name*")]
+        [Required]
+        public string LastName { get; set; }
+
+        [StringLength(30)]
+        [DisplayName("Email Address*")]
+        [Required]
+        public string Email { get; set; }
+
+        [StringLength(15)]
+        [DisplayName("Phone Number*")]
+        [Required]
+        public string Phone { get; set; }
+
+        [DisplayName("Years of Exprience*")]
+        [Required]
+        public int? Experience { get; set; }
+    }
+}
